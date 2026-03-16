b6594d6cb9c27c4d52830a46ad597ca60fc612e6
Nitesh pandey <140426447+Niteshpandey80@users.noreply.github.com>
2026-03-16 13:07:36 +0530

    new changes in project 4

--- project4/src/App.jsx
+++ project4/src/App.jsx
@@ -9,6 +9,7 @@ const App = () => {
   return (
     <div className='flex items-center justify-center h-screen bg-gray-100'>
       <div className='bg-white p-8 rounded-xl shadow-lg text-center'>
+        <h1 className='mt-2 mb-6 text-lg text-gray-700'>Hide And Show Button App </h1>
            <button onClick={toggleText} className='px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600'>
             {show ? "Hide Message " : "Show Message"}
            </button>
