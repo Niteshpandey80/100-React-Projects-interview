f16a297622b8f200bc78b81c7ce4d7be84a4153c
Nitesh pandey <140426447+Niteshpandey80@users.noreply.github.com>
2026-05-04 02:06:17 +0530

    making changes in project6

--- project6lightanddark/src/components/Home.jsx
+++ project6lightanddark/src/components/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
 
   return (
        <div className={`h-screen flex flex-col items-center justify-center ${dark ? "bg-black text-white " : "bg-white text-black"} `}>
-         <h1 className={`text-xl ${ dark ? 'text-white ' : 'text-black'}`}>This is toggle </h1>
+         <h1 className={`text-xl ${ dark ? 'text-white ' : 'text-black'}`}>Light Mode and Dark Mode  </h1>
          <button className={`mt-5 rounded px-4 py-3 ${dark ? "bg-white text-black " : "bg-black text-white"}`} onClick={handleToggle} >Toggle Button</button>
        </div>
   )
